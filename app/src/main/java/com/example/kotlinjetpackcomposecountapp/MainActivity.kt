e3845f4707580cfa5fa6ade508ac470fc6ad2860
yuto ichinose <yuto.0526.sano@gmail.com>
2023-07-25 13:39:34 +0900

    色々試しでつけた

--- app/src/main/java/com/example/kotlinjetpackcomposecountapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinjetpackcomposecountapp/MainActivity.kt
@@ -12,21 +12,30 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.kotlinjetpackcomposecountapp.ui.theme.KotlinJetpackComposeCountAppTheme
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        setContent { // setContent内でComposable関数を受け取る、Composable関数の中からComposable関数を呼び出せない。
             KotlinJetpackComposeCountAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -45,13 +54,21 @@ fun CounterApp() {
 
     val viewModel: CounterViewModel = viewModel()
 
-
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Count: ${viewModel.getCount()}", style = MaterialTheme.typography.bodyMedium)
+        Column {
+//            SimpleFilledTextFieldSample()
+            Text(
+                text = "Count: ${viewModel.getCount()}",
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.DarkGray, // 色を変える
+                fontSize = 18.sp
+            )
+        }
+
 
         Row(
             modifier = Modifier.padding(vertical = 16.dp),
@@ -86,11 +103,23 @@ fun CounterButton(text: String, onClick: () -> Unit) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SimpleFilledTextFieldSample() {
+    var text by remember { mutableStateOf("Welcome Summer") }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Greet Label") }
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewCounterButton () {
     CounterButton(
-        text = "+",
+        text = stringResource(R.string.hello_text), // リソースファイルから文字列を取得（ハードコードを避けるため）
         onClick = {}
     )
 }
