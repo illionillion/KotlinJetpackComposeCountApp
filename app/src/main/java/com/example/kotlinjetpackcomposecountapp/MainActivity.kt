b05c2e52bf07df548502f7b403b722bd052fab4e
yuto ichinose <yuto.0526.sano@gmail.com>
2023-07-25 14:35:13 +0900

    画像の練習

--- app/src/main/java/com/example/kotlinjetpackcomposecountapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinjetpackcomposecountapp/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.kotlinjetpackcomposecountapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.CheckCircle
@@ -35,7 +41,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -131,6 +139,44 @@ fun CounterButton(text: String, onClick: () -> Unit) {
         )
         Spacer(Modifier.size(ButtonDefaults.IconSpacing))
         Text(text = text, style = MaterialTheme.typography.bodyMedium)
+    }
+}
+
+@Composable
+fun ImageSample() {
+    val imageModifier = Modifier
+        .size(200.dp)
+//        .fillMaxWidth(0.5f) // 画面の半分のサイズ
+        .border(BorderStroke(3.dp, Color.Cyan))
+
+    Image(painter = painterResource(R.drawable.my_profile_picture), contentDescription = "サンプル画像", modifier = imageModifier)
+}
+
+@Composable
+fun ImageCircleSample() {
+    val imageModifier = Modifier
+        .size(200.dp)
+        .clip(CircleShape) // 円にする
+
+    Image(painter = painterResource(R.drawable.my_profile_picture), contentDescription = "サンプル画像", modifier = imageModifier)
+}
+
+@Composable
+fun ImageRoundedCornerShapeSample() {
+    val imageModifier = Modifier
+        .size(200.dp)
+        .clip(RoundedCornerShape(24.dp)) // 角を丸くする
+
+    Image(painter = painterResource(R.drawable.my_profile_picture), contentDescription = "サンプル画像", modifier = imageModifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewImageSamples() {
+    Column {
+        ImageSample()
+        ImageCircleSample()
+        ImageRoundedCornerShapeSample()
     }
 }
 
